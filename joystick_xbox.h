6972b942cd74c9071c2557d6d476ed43ae245161
lalalala256 <jtwang256@163.com>
2023-05-14 10:33:36 +0800

    Init version

---
+++ joystick_xbox.h
@@ -0,0 +1,39 @@
+#include <linux/input.h>
+#include <linux/joystick.h>
+#include <string>
+
+class JoystickXBox
+{
+public:
+    JoystickXBox(const std::string &dev_name);
+    ~JoystickXBox();
+    bool Open();
+    void Close();
+    bool Read(struct js_event &js);
+    unsigned char GetAxes()
+    {
+        return axes_;
+    }
+    unsigned char GetButtons()
+    {
+        return buttons_;
+    }
+    int GetFd()
+    {
+   		return fd_;
+    }
+    void PrintData();
+    void ProcessData(const struct js_event &js);
+
+private:
+    bool debug_ = true;
+    int fd_ = -1;
+    std::string dev_name_ = "";
+    int version_ = 0x000800;
+    char name_[512] = "Unkown";
+    unsigned char axes_ = 2;
+    unsigned char buttons_ = 2;
+    int *axis_ = nullptr;
+    char *button_ = nullptr;
+};
+
